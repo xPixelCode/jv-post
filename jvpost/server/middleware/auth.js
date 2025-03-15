6473a41202e825b682132da919ae0fefb5d6878b
xPixelCode <156606037+xPixelCode@users.noreply.github.com>
2025-03-15 22:31:44 +0200

    Add files via upload

---
+++ jvpost/server/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(403).send("Sinulla ei ole oikeutta tähän.");
+    }
+
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
